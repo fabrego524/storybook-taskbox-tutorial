5445a486015311c3d0cf8dea958dd8050ddefc16
Fabian Abrego <FabianA@quinnox.com>
2024-01-08 16:43:59 -0600

    Changed task background to lightgray

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -32,7 +32,7 @@ import { Task } from '../models/task.model';
                 id="title-{{ task?.id }}"
                 name="title-{{ task?.id }}"
                 placeholder="Input title"
-                style="background-color: blue;"
+                style="background-color: LightGray;"
             />
         </label>   
         <button
